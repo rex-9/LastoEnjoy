c4f3b1961a3bb26676362195439f967f233e3583
rex-9 <htetnaing0814@gmail.com>
2023-10-02 13:34:42 +0630

    fix login behavior

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import { useEffect, useState } from "react";
 
 const App = () => {
-  const [, setUser] = useState<string | null>();
+  const [user, setUser] = useState<string | null>();
   useEffect(() => {
     const user = localStorage.getItem("user") ?? null;
     setUser(user);
@@ -22,8 +27,19 @@ const App = () => {
     <Router>
       <div className="bg-gradient-to-br from-black via-red-300 to-blue-900 text-white font-mooli flex flex-col justify-center items-center w-screen h-screen">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          {/* <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} /> */}
+
+          <Route
+            path="/"
+            element={
+              user === undefined ? <Navigate to="/login" replace /> : <Home />
+            }
+          />
+          <Route
+            path="/login"
+            element={user ? <Navigate to="/" replace /> : <Login />}
+          />
         </Routes>
       </div>
     </Router>
